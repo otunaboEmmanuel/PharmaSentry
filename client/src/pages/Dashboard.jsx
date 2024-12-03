648f9ba03707a3e727917276c83dd863594d2484
otunaboEmmanuel <otunabochidubem@gmail.com>
2024-12-03 16:09:12 +0100

    UI changes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -183,7 +183,7 @@ const Dashboard = () => {
                         <tr key={index} className="hover:bg-gray-100">
                           <td className="py-2 px-4 border-b text-left">{`${patient.FirstName} ${patient.LastName}`}</td>
                           <td className="py-2 px-4 border-b text-left">{calculateAge(patient.DateOfBirth)}</td>
-                          <td className="py-2 px-4 border-b text-left">{ }</td>
+                          <td className="py-2 px-4 border-b text-left">{}</td>
                           <td className="py-2 px-4 border-b text-left">{patient.Status}</td>
                           <td className="py-2 px-4 border-b text-left">
                             <button
