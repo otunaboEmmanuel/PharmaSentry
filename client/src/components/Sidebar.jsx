3bb63173cebb7568929e91994c3c3492f9d30ad6
otunaboEmmanuel <otunabochidubem@gmail.com>
2024-12-03 17:35:57 +0100

    UI changes

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import SidebarItems from './SidebarItems';
 const Sidebar = () => {
     return (
         <div className="fixed flex-col w-64 min-h-screen bg-white items-center hidden sm:flex">
-            <div className="flex flex-row w-full justify-center items-center mt-8">
+            <div className="flex flex-row w-full justify-center items-center mt-8 mb-8">
                 <img width="48" height="48" src="https://img.icons8.com/emoji/48/hospital-emoji.png" alt="hospital-emoji" />
                 <h3 className="text-[#663399]  items-center font-bold text-2xl">PharmaSentry</h3>
             </div>
